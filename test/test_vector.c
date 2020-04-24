f362217775e0c904f392f75c96c742158e40502d
Jacky <gomjellie@gmail.com>
2020-04-24 21:13:34 +0900

    add char* tests

--- test/test_vector.c
+++ test/test_vector.c
@@ -7,7 +7,7 @@
 vector* v;
 
 void setUp(void) {
-    v = new_vector(sizeof(int));
+    
 }
 
 void tearDown(void) {
@@ -15,34 +15,54 @@ void tearDown(void) {
 }
 
 void test_vector_set_with_int(void) {
+    v = new_vector(sizeof(int));
     int nine = 9;
     vector_set(v, 2, &nine);
 }
 
 void test_vector_get_with_int(void) {
+    v = new_vector(sizeof(int));
     int ten = 10;
     vector_set(v, 130, &ten);
     int* res = (int*)vector_get(v, 131);
-    if (res == NULL) { // supposed to be NULL
-        printf("pass"); return;
-    }
-
-    int zero = 0;
-    int err = 10 / zero; // throw errow 
+    TEST_ASSERT_TRUE (res == NULL);
 }
 
 void test_vector_has(void) {
+    v = new_vector(sizeof(int));
     int nine = 9;
     vector_set(v, 2, &nine);
     
     bool has_9 = vector_has(v, &nine);
-    if (has_9) { // supposed to have nine
-        printf("pass"); return;
-    }
+    TEST_ASSERT_TRUE (has_9);
 }
 
 void test_vector_index(void) {
+    v = new_vector(sizeof(int));
     int nine = 9;
     vector_set(v, 2, &nine);
-    printf("nine is at %d\n", vector_index(v, &nine));
+    TEST_ASSERT_TRUE (vector_index(v, &nine) == 2);
+}
+
+void test_vector_new_with_string(void) {
+    v = new_vector(sizeof(char *));
+}
+
+void test_vector_set_with_string(void) {
+    v = new_vector(sizeof(char *));
+    char hello[32] = "hello";
+    vector_set(v, 0, hello);
+    hello[0] = 'q';
+    char* res;
+    res = vector_get(v, 0);
+    printf("%p\n", hello);
+    printf("%p", res);
+}
+
+void test_vector_has_with_string(void) {
+    v = new_vector(sizeof(char *));
+    char hello[32] = "hello";
+    vector_set(v, 32, hello);
+    char find_hello[32] = "hello";
+    printf("%d", vector_index(v, find_hello));
 }
